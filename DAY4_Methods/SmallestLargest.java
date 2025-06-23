4d6b27af45b692ace853c4cbd69d825c748093c4
@vishnuBhai16 <debeloperweb@gmail.com>
2025-06-23 14:50:04 +0530

    SmallestLargest  added

---
+++ DAY4_Methods/SmallestLargest.java
@@ -0,0 +1,43 @@
+package Day_4_Methods;
+
+import java.util.*;
+public class SmallestLargest {
+
+    public static int[] findSmallestAndLargest(int number1, int number2, int number3){
+        int largest = number1;
+        int smallest = number1;
+        if (number2 < smallest) {
+            smallest = number2;
+        }
+        if (number3 < smallest) {
+            smallest = number3;
+        }
+
+        if (number2 > largest) {
+            largest = number2;
+        }
+        if (number3 > largest) {
+            largest = number3;
+        }
+
+        return new int[]{smallest, largest};
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int num3 = sc.nextInt();
+
+        int[] result = findSmallestAndLargest(num1, num2, num3);
+
+        System.out.println("Smallest number: " + result[0]);
+        System.out.println("Largest number: " + result[1]);
+
+    }
+}
