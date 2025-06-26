0bd4b94008e2ec48f8a3ffe0bac897e07664c2a6
@vishnuBhai16 <debeloperweb@gmail.com>
2025-06-26 08:52:53 +0530

    Added   vehicle.java

---
+++ W2_DAY2_/Vehicle.java
@@ -0,0 +1,28 @@
+package W2_DAY2_;
+public class Vehicle {
+    // Instance Variables
+    private String ownerName;
+    private String vehicleType;
+
+    // Class Variable
+    private static double registrationFee = 500.0; // Default fee for all vehicles
+
+    // Constructor
+    public Vehicle(String ownerName, String vehicleType) {
+        this.ownerName = ownerName;
+        this.vehicleType = vehicleType;
+    }
+
+    // Instance Method: Display vehicle details
+    public void displayVehicleDetails() {
+        System.out.println("Owner Name: " + ownerName);
+        System.out.println("Vehicle Type: " + vehicleType);
+        System.out.println("Registration Fee: ₹" + registrationFee);
+    }
+
+    // Class Method: Update registration fee
+    public static void updateRegistrationFee(double newFee) {
+        registrationFee = newFee;
+    }
+}
+
